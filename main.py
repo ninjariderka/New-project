d9a99177caa33ccec1d9d541ad0b1ffd0f83fef2
ninjariderka <uchman.jagoda@gmail.com>
2023-07-01 11:58:16 +0200

    Fix is_prime function

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ def is_prime(n):
 
 	if n == 2:
 		return True
+	if n % 2 == 0:
+		return False
 
-	for i in range(2, n):
-		if n % 2 ==0:
+	for i in range(3, int(n ** 0.5) + 1, 2):
+		if n % i == 0:
 			return False
 	return True
 
